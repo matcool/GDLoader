ecc2c93095fac9eab738aa6695618755e7bc1430
matcool <26722564+matcool@users.noreply.github.com>
2021-05-29 15:41:47 -0300

    defer mods in settings.txt

--- src/GDLoader.cpp
+++ src/GDLoader.cpp
@@ -6,8 +6,15 @@
 #include <filesystem>
 #include <fstream>
 #include <algorithm>
+#include <unordered_set>
+#include <cctype>
 
 EXPORT void zzz_gdloader_stub() {}
+
+std::string& lower_str(std::string& str) {
+    std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c){ return std::tolower(c); });
+    return str;
+}
 
 namespace GDLoader {
     std::vector<std::shared_ptr<Mod>> g_mods;
@@ -60,6 +67,21 @@ namespace GDLoader {
         return mod;
     }
 
+    std::unordered_set<std::wstring> g_loadLater;
+
+    bool (__thiscall* LoadingLayer_init)(void*, bool);
+    bool __fastcall LoadingLayer_init_H(void* self, void*, bool unk) {
+        if (!LoadingLayer_init(self, unk))
+            return false;
+
+        for (const auto& path : g_loadLater) {
+            LoadLibraryW(path.c_str());
+        }
+        g_loadLater.clear();
+
+        return true;
+    }
+
     void init() {
         MH_Initialize();
         // TODO: maybe make some way to make mods load one at a time
@@ -70,12 +92,15 @@ namespace GDLoader {
         std::filesystem::path basePath = _basePath;
         basePath = basePath.parent_path();
 
+        std::unordered_set<std::string> loadLaterNames;
+
         if (std::filesystem::exists(basePath / "settings.txt")) {
             std::ifstream file(basePath / "settings.txt");
             std::string line;
             while (std::getline(file, line)) {
-                // lol
+                loadLaterNames.insert(lower_str(line));
             }
+            file.close();
         }
 
         auto modsPath = basePath / "mods";
@@ -85,17 +110,25 @@ namespace GDLoader {
         for (const auto& entry : std::filesystem::directory_iterator(modsPath)) {
             const auto& path = entry.path();
             if (path.extension() == ".dll") {
-                LoadLibraryW(path.wstring().c_str());
+                auto filename = path.filename().string();
+                if (loadLaterNames.find(lower_str(filename)) != loadLaterNames.end()) {
+                    g_loadLater.insert(path.wstring());
+                } else {
+                    LoadLibraryW(path.wstring().c_str());
+                }
             }
         }
-        // MH_CreateHookEx(0, );
+        const auto addr = reinterpret_cast<char*>(GetModuleHandle(0)) + 0x18c080;
+        MH_CreateHookEx(0,
+            addr,
+            LoadingLayer_init_H,
+            reinterpret_cast<void**>(&LoadingLayer_init)
+        );
+        MH_EnableHookEx(0, addr);
     }
 }
 
 DWORD WINAPI DllThread(void* module) {
-    // idek what this is for but cos8o mentioned it
-    // he also went on for 10 more minutes on CreateThread
-    // although i wasnt paying attention
     DisableThreadLibraryCalls(reinterpret_cast<HMODULE>(module));
     GDLoader::init();
     return 0;
